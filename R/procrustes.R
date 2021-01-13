84b784cb53fc09149585f5e4ab008c83a735212c
cristianpachon <cc.pachon@gmail.com>
2021-01-13 14:03:07 +0100

    Add procrustes file

---
+++ R/procrustes.R
@@ -0,0 +1,22 @@
+perform_procrustes <- function(x, target, matrix_to_transform, translation, dilation) {
+
+  procrustes_result <- MCMCpack::procrustes(X = x, Xstar = target, translation = translation, dilation = dilation)
+
+  if (translation) {
+    trans <- procrustes_result$tt
+  } else {
+    trans <- matrix(data = 0, nrow = ncol(x), ncol = 1)
+  }
+
+  ones_vector <- matrix(data = 1, nrow = nrow(matrix_to_transform), ncol = 1)
+  translation_matrix <- ones_vector %*% t(trans)
+
+  if (dilation) {
+    dilation_factor <- procrustes_result$s
+  } else {
+    dilation_factor <- 1
+  }
+
+  return(dilation_factor * matrix_to_transform %*% procrustes_result$R + translation_matrix)
+
+}
