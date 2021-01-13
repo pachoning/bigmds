c2845463f24e86483b59742bc0f0b7877bb038ed
cristianpachon <cc.pachon@gmail.com>
2021-01-13 15:04:28 +0100

    Add tests for classical_mds method

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -5,3 +5,12 @@ test_that("Procrustes works", {
   max_error <- max(y - x_proc)
   expect_lt(max_error, 1e-6)
 })
+
+test_that("Classical MDS works", {
+  x <- matrix(data = rnorm(4*100, sd = 10), nrow = 100)
+  cmds <- classical_mds(x = x, k = 4)
+  cmds_proc <- perform_procrustes(x = cmds$points, target = x, matrix_to_transform = cmds$points, 
+                                  translation = FALSE, dilation = FALSE)
+  corr_first <- cor(x[, 1], cmds_proc[, 1])
+  expect_gt(corr_first, 0.9)
+})
