637f6047a010e4bd38288e72a84f0512545e8d6b
cristianpachon <cc.pachon@gmail.com>
2021-01-22 16:03:46 +0100

    Take into account matrix_to_transform in perform_procrustes function

--- R/utils.R
+++ R/utils.R
@@ -1,45 +1,52 @@
 perform_procrustes <- function(x, target, matrix_to_transform, translation = FALSE, dilation = FALSE) {
-  
+
   n_row <- nrow(x)
   n_col <- ncol(x)
-  
+
   if (n_row != nrow(target)) {
     stop("x and target do not have same number of rows.\n")
   }
-  
+
   if (n_col != ncol(target)) {
     stop("x and target do not have same number of columns.\n")
   }
   
+  if (n_col != ncol(matrix_to_transform)) {
+    stop("x and matrix_to_transform do not have same number of columns.\n")
+  }
+
   diag_matrix <- diag(n_row)
   if (translation) {
     diag_matrix <- diag(n_row) - 1/n_row * matrix(1, n_row, n_row)
   }
-  
+
   matrix_prod <- t(target) %*% diag_matrix %*% x
   svd_results <- svd(matrix_prod)
   rotation_matrix <- svd_results$v %*% t(svd_results$u)
-  
+
   dilation_factor <- 1
   if (dilation) {
     mat1 <- t(target) %*% diag_matrix %*% x %*% rotation_matrix
     mat2 <- t(x) %*% diag_matrix %*% x
     num <- 0
     denom <- 0
+
     for (i in 1:n_col) {
       num <- num + mat1[i, i]
       denom <- denom + mat2[i, i]
     }
+
     dilation_factor <- num/denom
   }
-  
-  translation_matrix <- matrix(0, n_col, 1)
+
+  translation_vector <- matrix(0, n_col, 1)
   if (translation) {
-    translation_matrix <- 1/n_row * t(target - dilation_factor * x %*% rotation_matrix) %*% matrix(1, n_row, 1)
+    translation_vector <- 1/n_row * t(target - dilation_factor * x %*% rotation_matrix) %*% matrix(1, n_row, 1)
   }
-  
-  translation_matrix <- matrix(translation_matrix, n_row, n_col, byrow = TRUE)
-  
+
+  ones_vector <- matrix(data = 1, nrow = nrow(matrix_to_transform), ncol = 1)
+  translation_matrix <- ones_vector %*% t(translation_vector)
+
   return(dilation_factor * matrix_to_transform %*% rotation_matrix + translation_matrix)
 }
 
