369b524898c66d1ab44cc156c0ffcda16d446228
cristianpachon <cc.pachon@gmail.com>
2021-01-27 13:57:45 +0100

    Allow random sampling for gower_interpolation_mds method

--- R/gower_interpolation_mds.R
+++ R/gower_interpolation_mds.R
@@ -37,9 +37,12 @@ gower_interpolation_mds <- function(x, l, k, dist_fn = stats::dist) {
   } 
 
   if (p > 1) {
+
+    initial_row_names <- row.names(x)
+    row.names(x) <- 1:nrow(x)
+    
     # Do MDS with the first group and then use the Gower interpolation formula
-    sample_distribution <- sort(sample(x = p, size = nrow_x, replace = TRUE))
-    sample_distribution <- sort(sample_distribution)
+    sample_distribution <- sample(x = p, size = nrow_x, replace = TRUE)
 
     # Get the first group 
     ind_1 <- which(sample_distribution == 1)
@@ -80,8 +83,14 @@ gower_interpolation_mds <- function(x, l, k, dist_fn = stats::dist) {
       A <- distance_matrix_filter^2
       ones_vector <- rep(1, length(ind_i_group))
       MDS_i_group <- 1 / (2 * n_1) * (ones_vector %*% t(g_vector) - A) %*% M %*% S_inv
+      row.names(MDS_i_group) <- row.names(submatrix_data)
       cum_mds <- rbind(cum_mds, MDS_i_group)
     }
+
+    cum_mds <- cum_mds[order(as.numeric(row.names(cum_mds))), , drop = FALSE]
+    row.names(cum_mds) <- initial_row_names
+    row.names(x) <- initial_row_names
+
   } else {
     # It is possible to run MDS directly
     mds_eig <- classical_mds(x = x, k = k, dist_fn = dist_fn, return_distance_matrix = TRUE)
