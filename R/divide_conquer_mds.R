ab1aea4612d6808be47770a2581b42cb979545a8
cristianpachon <cc.pachon@gmail.com>
2021-02-08 15:52:24 +0100

    Use apply methods to speed up divide_conquer_mds method

--- R/divide_conquer_mds.R
+++ R/divide_conquer_mds.R
@@ -1,26 +1,51 @@
 get_partitions_for_divide_conquer <- function(n, l, tie, k) {
 
-  p <- ceiling(n / (l-tie))
-  min_sample_size <- max(k + 2, tie)
+  size_partition <- l-tie
+  p <- ceiling(n/size_partition)
+  min_sample_size <- max(k, tie)
+  partition_sample_size <- l-tie
+  last_sample_size <- n-(p-1)*partition_sample_size
+  list_indexes <- list()
 
-  index_partition <- sort(rep(x = 1:p, length.out = n, each = ceiling(n / p)))
-  p <- max(index_partition)
-
-  if (mean(table(index_partition)) < min_sample_size) {
-    stop("Too many columns and few observations to perform Divide and Conquer MDS")
+  if (l-tie <= 0) {
+    stop("l must be greater than tie")
+  } else if(l-tie <= tie) {
+    stop("l-tie must be greater than tie")
+  } else if(l-tie <= k) {
+    stop("l-tie must be greater than k")
   }
 
-  while ((min(table(index_partition)) < min_sample_size) & (p <= n)) {
-    p <- p + 1
-    index_partition <- sort(rep(x = 1:p, length.out = n, each = ceiling(n / p)))
+  if (last_sample_size < min_sample_size & last_sample_size > 0) {
+    p <- p - 1
   }
 
-  if (min(table(index_partition)) < min_sample_size) {
-    stop("Partitions for divide and conquer suffer from lacking of data")
+  for (i in 1:p) {
+    if (i == 1) {
+      ini <- 1
+      end <- size_partition
+    } else if (i < p) {
+      ini <- end + 1
+      end <- (ini-1) + size_partition
+    } else {
+      ini <- end + 1
+      end <- n
+    }
+
+    list_indexes[[i]] <- ini:end
   }
 
-  return(index_partition)
+  return(list_indexes)
+
 }
+
+
+divide_matrix <- function(x, long) {
+  n_row <- nrow(x)
+  x_first <- x[1:long, , drop = FALSE]
+  x_rest <- x[(long+1):n_row, , drop = FALSE]
+  return(list(first = x_first, rest = x_rest))
+}
+
 
 #'@title Divide and Conquer MDS
 #'@description Performs *Multidimensional Scaling* for big datasets using a Divide and Conquer strategy. This method can 
@@ -67,60 +92,71 @@ get_partitions_for_divide_conquer <- function(n, l, tie, k) {
 #'@export
 divide_conquer_mds <- function(x, l, tie, k, dist_fn = stats::dist, ...) {
 
-  initial_row_names <- row.names(x)
-  row.names(x) <- 1:nrow(x)
+  n_row_x <- nrow(x)
 
-  if (nrow(x) <= l) {
+  if (n_row_x <= l) {
     mds_to_return <- classical_mds(x = x, k = k, dist_fn = dist_fn, ...)
-    mds_to_return$eigen <- mds_to_return$eigen / length(mds_to_return$eigen)
+    mds_to_return$eigen <- mds_to_return$eigen / nrow(x)
+
   } else {
-    index_partition <- get_partitions_for_divide_conquer(n = nrow(x), l = l, tie = tie, k = k)
-    p <- max(index_partition)
+    
+    # Generate indexes list. Each element correspond to the index of the partition
+    idx <- get_partitions_for_divide_conquer(n = n_row_x, l = l, tie = tie, k = k)
+    num_partitions <- length(idx)
 
-    min_len <- NA
-    eigen <- c()
+    # Get the elements of the first partition and drop from the list
+    idx_1 <- idx[[1]]
+    idx[[1]] <- NULL 
 
-    # Calculate mds for each partition and take tie poits from each subsample
-    for (i in 1:p) {
+    # Partition x
+    x_partition <- lapply(idx, function(rows, matrix) matrix[rows, , drop = FALSE], matrix = x)
 
-      indexes_current <- which(index_partition == i)
-      x_current <- x[indexes_current, ,drop = FALSE]
-      row_names_current <- row.names(x_current)
+    # Take a sample from the first partition
+    sample_first_partition <- sample(x = idx_1, size = tie, replace = FALSE)
+    x_1 <- x[idx_1, , drop = FALSE]
+    x_sample_1 <- x_1[sample_first_partition, , drop = FALSE]
 
-      if (i == 1) {
+    # Join each partition with the sample from the first partition
+    x_join_1 <- lapply(x_partition, function(m_big, m_small) rbind(m_small, m_big), m_small = x_sample_1)
 
-        list_classical_mds <- classical_mds(x = x_current, k = k, dist_fn = dist_fn, ...) 
-        cum_mds <- list_classical_mds$points
-        eigen <- list_classical_mds$eigen / length(list_classical_mds$eigen)
-        min_len <- length(eigen)
-      } else {
+    # Perform MDS for each partition as well as for the first partition
+    mds_1 <- classical_mds(x = x_1, k = k, dist_fn = dist_fn, return_distance_matrix = FALSE, ...)
+    mds_1_points <- mds_1$points
+    mds_1_eigen <- mds_1$eigen/nrow(x_1)
+    mds_1_sample <- mds_1_points[sample_first_partition, ,drop = FALSE]
 
-        list_mds_both <- classical_mds(x = x[c(rn_subsample_previous, row_names_current), ,drop = FALSE], k = k)
-        mds_both <- list_mds_both$points
-        mds_both_previous <- mds_both[rn_subsample_previous, ,drop = FALSE]
-        mds_both_current <- mds_both[row_names_current, ,drop = FALSE]
-        cum_mds_previous <- cum_mds[rn_subsample_previous, ,drop = FALSE]
-        mds_current_aligned <- perform_procrustes(x = mds_both_previous, target = cum_mds_previous,
-                                                  matrix_to_transform = mds_both_current, 
-                                                 translation = FALSE, dilation = FALSE)
-        cum_mds <- rbind(cum_mds, mds_current_aligned)
-        min_len <- pmin(min_len, length(list_mds_both$eigen))
-        eigen <- eigen[1:min_len] + (list_mds_both$eigen[1:min_len] / length(list_mds_both$eigen))
-      }
+    mds_join_1 <- lapply(x_join_1, classical_mds, k = k, dist_fn = dist_fn, return_distance_matrix = FALSE)
+    mds_join_1_points <- lapply(mds_join_1, function(x) x$points)
+    mds_join_1_eigen <- lapply(mds_join_1, function(x) x$eigen)
 
-      rn_subsample_previous <- sample(x = row_names_current, size = tie, replace = FALSE)
+    # For each partition, divide the matrix into two part: 
+    ## first corresponding to the sample of the first partition
+    # the rest of the matrix corresponding to the observations of each matrix
+    mds_division <- lapply(mds_join_1_points, divide_matrix, long = tie)
+    mds_division_first <- lapply(mds_division, function(x) x$first)
+    mds_division_rest <- lapply(mds_division, function(x) x$rest)
 
-    }
+    # Apply Procrustes for each partition
+    mds_procrustes <- mapply(FUN = perform_procrustes, 
+                             x = mds_division_first, 
+                             matrix_to_transform = mds_division_rest,
+                             MoreArgs = list(target = mds_1_sample, translation = FALSE, dilation = FALSE))
 
-    # Perform the mean for the eigenvalues
-    eigen <- eigen / p
+    # Join all the solutions
+    mds_solution <- Reduce(rbind, mds_procrustes)
+    mds_solution <- Reduce(rbind, list(mds_1_points, mds_solution))
 
-    # Divide by the number of observations
-    mds_to_return <- list(points = cum_mds, eigen = eigen)
+    # Get eigenvalues
+    eigen <- mapply(function(x, y) x/length(y), x = mds_join_1_eigen, y = idx)
+    eigen <- Reduce(`+`, eigen)
+
+    mds_1_eigen <- mds_1_eigen/length(mds_1_eigen)
+
+    eigen <- Reduce(`+`, list(eigen, mds_1_eigen))
+    eigen <- eigen/num_partitions
+
+    mds_to_return <- list(points = mds_solution, eigen = eigen)
   }
-
-  row.names(x) <- initial_row_names
-  row.names(mds_to_return$points) <- initial_row_names
 
   return(mds_to_return)
 }
