6c38b577ee32987f1270590d253317d2f9e7db34
cristianpachon <cc.pachon@gmail.com>
2021-08-03 12:08:37 +0200

    Update Procrustes function

--- R/utils.R
+++ R/utils.R
@@ -1,61 +1,63 @@
-perform_procrustes <- function(x, target, matrix_to_transform, translation = FALSE, dilation = FALSE) {
+get_procrustes_parameters <- function(x, target, translation = FALSE) {
+
+  n_row <- nrow(x)
+  n_col <- ncol(x)
+
+  if (translation) {
+    c.target <- scale(target, center = TRUE, scale = FALSE)
+    m.target <- attr(c.target, "scaled:center")
+
+    c.x <- scale(x, center = TRUE, scale = FALSE)
+    m.x <- attr(c.x, "scaled:center")
+
+    matrix_prod <- t(c.target) %*% x
+    svd_results <- svd(matrix_prod)
+    rotation_matrix <- svd_results$v %*% t(svd_results$u)
+
+    translation_vector <- m.target - t(rotation_matrix) %*% m.x
+
+  } else {
+    matrix_prod <- t(target) %*% x
+    svd_results <- svd(matrix_prod)
+    rotation_matrix <- svd_results$v %*% t(svd_results$u)
+    translation_vector <- matrix(data = 0, nrow = n_col, ncol = 1)
+  }
+
+  return(list(rotation_matrix = rotation_matrix, translation_vector = translation_vector))
+}
+
+perform_procrustes <- function(x, target, matrix_to_transform, translation = FALSE) {
 
   n_row <- nrow(x)
   n_col <- ncol(x)
 
   if (n_row != nrow(target)) {
-    stop("x and target do not have same number of rows.\n")
+    stop("\"x\" and \"target\" do not have same number of rows")
   }
 
   if (n_col != ncol(target)) {
-    stop("x and target do not have same number of columns.\n")
+    stop("\"x\" and \"target\" do not have same number of columns")
   }
 
   if (n_col != ncol(matrix_to_transform)) {
-    stop("x and matrix_to_transform do not have same number of columns.\n")
+    stop("\"x\" and \"matrix_to_transform\" do not have same number of columns")
   }
 
-  diag_matrix <- diag(n_row)
-  if (translation) {
-    diag_matrix <- diag(n_row) - 1/n_row * matrix(1, n_row, n_row)
-  }
-
-  matrix_prod <- t(target) %*% diag_matrix %*% x
-  svd_results <- svd(matrix_prod)
-  rotation_matrix <- svd_results$v %*% t(svd_results$u)
-
-  dilation_factor <- 1
-  if (dilation) {
-    mat1 <- t(target) %*% diag_matrix %*% x %*% rotation_matrix
-    mat2 <- t(x) %*% diag_matrix %*% x
-    num <- 0
-    denom <- 0
-
-    for (i in 1:n_col) {
-      num <- num + mat1[i, i]
-      denom <- denom + mat2[i, i]
-    }
-
-    dilation_factor <- num/denom
-  }
-
-  translation_vector <- matrix(0, n_col, 1)
-  if (translation) {
-    translation_vector <- 1/n_row * t(target - dilation_factor * x %*% rotation_matrix) %*% matrix(1, n_row, 1)
-  }
+  procrustes_parameters <- get_procrustes_parameters(x = x, target = target, translation = translation)
 
   ones_vector <- matrix(data = 1, nrow = nrow(matrix_to_transform), ncol = 1)
-  translation_matrix <- ones_vector %*% t(translation_vector)
+  translation_matrix <- matrix(data = 0, nrow = nrow(matrix_to_transform), ncol = ncol(matrix_to_transform))
+  translation_matrix <- translation_matrix + ones_vector %*% t(procrustes_parameters$translation_vector)
 
-  return(dilation_factor * matrix_to_transform %*% rotation_matrix + translation_matrix)
+  return(matrix_to_transform %*% procrustes_parameters$rotation_matrix + translation_matrix)
 }
 
 classical_mds <- function(x, k, dist_fn, return_distance_matrix = FALSE, ...) {
 
   if (!is.function(dist_fn)) {
-    stop("dist_fn must be a function")
+    stop("\"dist_fn\" must be a function")
   } else if (any(is.na(x))) {
-    stop("There are some NA values in the data. Please remove them")
+    stop("There are some \"NA\" values in the data. Please remove them")
   }
 
   mds <- list()
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,16 +1,90 @@
-#test_that("Procrustes aligns matrices", {
-#  x <- matrix(data = c(1, 2, 3, 4), nrow = 2)
-#  y <- matrix(data = c(-1, -2, -3, -4), nrow = 2)
-#  x_proc <- perform_procrustes(x=x, target=y, matrix_to_transform=x, translation=FALSE, dilation=FALSE)
-#  max_error <- max(y - x_proc)
-#  expect_lt(max_error, 1e-6)
-#})
+test_that("Procrustes parameters are well recovered when translation vector is FALSE", {
+  n_rows <- 100
+  n_cols <- 2
+  x <- matrix(data = rnorm(n_rows*n_cols, sd = 10), nrow = n_rows)
+  rot_mat <- matrix(data = c(cos(45), -sin(45), sin(45), cos(45)), ncol = n_cols, nrow = n_cols)
+  y <- x %*% rot_mat
+  proc <- get_procrustes_parameters(x = x, target = y, translation = FALSE)
+  dif_rot <- max(abs(proc$rotation_matrix - rot_mat))
+  max_trans <- max(abs(proc$translation_vector))
+  max_error <- max(c(dif_rot, max_trans))
+  expect_lt(max_error, 1e-6)
+})
+
+test_that("Procrustes parameters are well recovered when translation vector is TRUE", {
+  n_rows <- 100
+  n_cols <- 2
+  x <- matrix(data = rnorm(n_rows*n_cols, sd = 10), nrow = n_rows)
+  rot_mat <- matrix(data = c(cos(45), -sin(45), sin(45), cos(45)), ncol = n_cols, nrow = n_cols)
+  trans_vector <- matrix(data = c(1, -1), nrow = n_cols, ncol = 1)
+  ones_vector <- matrix(data = 1, nrow = n_rows, ncol = 1)
+  trans_matrix <- ones_vector %*% t(trans_vector)
+  y <- x %*% rot_mat + trans_matrix
+  proc <- get_procrustes_parameters(x = x, target = y, translation = TRUE)
+  dif_rot <- max(abs(proc$rotation_matrix - rot_mat))
+  max_trans <- max(abs(proc$translation_vector - trans_vector))
+  max_error <- max(c(dif_rot, max_trans))
+  expect_lt(max_error, 1e-6)
+})
+
+test_that("Procrustes transforms the matrix correctly when translation vector is FALSE", {
+  n_rows <- 100
+  n_cols <- 2
+  x <- matrix(data = rnorm(n_rows*n_cols, sd = 10), nrow = n_rows)
+  rot_mat <- matrix(data = c(cos(45), -sin(45), sin(45), cos(45)), ncol = n_cols, nrow = n_cols)
+  y <- x %*% rot_mat
+  proc <- perform_procrustes(x = x, target = y, matrix_to_transform = x, translation = FALSE)
+  max_error <- max(abs(proc - y))
+  expect_lt(max_error, 1e-6)
+})
+
+test_that("Procrustes transforms the matrix correctly when translation vector is TRUE", {
+  n_rows <- 100
+  n_cols <- 2
+  x <- matrix(data = rnorm(n_rows*n_cols, sd = 10), nrow = n_rows)
+  rot_mat <- matrix(data = c(cos(45), -sin(45), sin(45), cos(45)), ncol = n_cols, nrow = n_cols)
+  trans_vector <- matrix(data = c(1, -1), nrow = n_cols, ncol = 1)
+  ones_vector <- matrix(data = 1, nrow = n_rows, ncol = 1)
+  trans_matrix <- ones_vector %*% t(trans_vector)
+  y <- x %*% rot_mat + trans_matrix
+  proc <- perform_procrustes(x = x, target = y, matrix_to_transform = x, translation = TRUE)
+  max_error <- max(abs(proc - y))
+  expect_lt(max_error, 1e-6)
+})
+
+test_that("Procrustes fails when number of rows does not match (x, target)", {
+  x <- matrix(data = c(1, 2, 3, 4, 5, 6), nrow = 3)
+  y <- matrix(data = c(-1, -2, -3, -4), nrow = 2)
+  expect_error(
+    perform_procrustes(x = x, target = y, matrix_to_transform = x, translation = FALSE),
+    "\"x\" and \"target\" do not have same number of rows"
+  )
+})
+
+test_that("Procrustes fails when number of columns does not match (x, target)", {
+  x <- matrix(data = c(1, 2, 3, 4, 5, 6), nrow = 2, ncol = 3)
+  y <- matrix(data = c(-1, -2, -3, -4), nrow = 2)
+  expect_error(
+    perform_procrustes(x = x, target = y, matrix_to_transform = x, translation = FALSE),
+    "\"x\" and \"target\" do not have same number of columns"
+  )
+})
+
+test_that("Procrustes fails when number of columns does not match (x, matrix_to_transform)", {
+  x <- matrix(data = c(1, 2, 3, 4, 5, 6), nrow = 3)
+  x_t <- t(x)
+  y <- matrix(data = c(-1, -2, -3, -4, -5, -6), nrow = 3)
+  expect_error(
+    perform_procrustes(x = x, target = y, matrix_to_transform = x_t, translation = FALSE),
+    "\"x\" and \"matrix_to_transform\" do not have same number of columns"
+  )
+})
 
 test_that("Classical MDS returns a valid MDS configuration", {
   x <- matrix(data = rnorm(4*100, sd = 10), nrow = 100)
   cmds <- classical_mds(x = x, k = 4, dist_fn = dist)
   cmds_proc <- perform_procrustes(x = cmds$points, target = x, matrix_to_transform = cmds$points, 
-                                  translation = FALSE, dilation = FALSE)
+                                  translation = FALSE)
   corr_first <- cor(x[, 1], cmds_proc[, 1])
   expect_gt(corr_first, 0.9)
 })
@@ -22,11 +96,11 @@ test_that("Classical MDS fails when distance is not provided", {
 
 test_that("Classical MDS fails when dist_fn is not a function", {
   x <- matrix(data = rnorm(4*100, sd = 10), nrow = 100)
-  expect_error(classical_mds(x = x, k = 4, dist_fn = 3), "dist_fn must be a function")
+  expect_error(classical_mds(x = x, k = 4, dist_fn = 3), "\"dist_fn\" must be a function")
 })
 
 test_that("Classical MDS fails when there are NA values", {
   x <- matrix(data = rnorm(4*100, sd = 10), nrow = 100)
   x[1,1] <- NA
-  expect_error(classical_mds(x = x, k = 4, dist_fn = dist), "There are some NA values in the data. Please remove them")
+  expect_error(classical_mds(x = x, k = 4, dist_fn = dist), "There are some \"NA\" values in the data. Please remove them")
 })
